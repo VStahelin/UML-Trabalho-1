349dd5d481d83b18a61ba969b9be013e6022f917
Vitor Stahelin <vitorjosestahelin@gmail.com>
2019-08-20 14:11:08 -0300

    Fix main class calculator

--- UML/src/Conversor.java
+++ UML/src/Conversor.java
@@ -1,15 +1,15 @@
 public class Conversor {
-    public static void main (){
+    public static void main (String[] a){
         Fahrenheint fahrenheint = new Fahrenheint();
-        fahrenheint.setTemperatura(10);
-        System.out.println(String.valueOf(fahrenheint.toCelsius()));
+        fahrenheint.setTemperatura(100);
+        System.out.println(String.valueOf("Em celsios fica: " + fahrenheint.toCelsius()));
     }
 }
 class Fahrenheint{
     private float temperatura;
 
     float toCelsius(){
-        float fahrenheint = (temperatura - 32) * (5/9);
+        float fahrenheint = (5/9) * (temperatura - 32);
         return fahrenheint;
     }
 
